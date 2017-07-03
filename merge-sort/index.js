3ad2f0694014b29d7d1e5772004467be07302121
Viking <olaf.cichocki@gmail.com>
2017-07-03 02:33:08 +0200

    merge-sort feat: Working version
    First, crude version

--- merge-sort/index.js
+++ merge-sort/index.js
@@ -1,7 +1,39 @@
 'use strict';
-function mergeSort(input) {
-    return input;
-}
-;
+const BASE_CASE_SIZE = 1;
+const mergeSort = (input) => {
+    const inputLength = input.length, inputIsSmallEnough = inputLength <= BASE_CASE_SIZE;
+    if (inputIsSmallEnough) {
+        return input;
+    }
+    const splittingPoint = Math.floor(inputLength / 2), leftArray = input.slice(0, splittingPoint), rightArray = input.slice(splittingPoint), sortedLeftArray = mergeSort(leftArray), sortedRightArray = mergeSort(rightArray);
+    return merge(sortedLeftArray, sortedRightArray);
+};
+const merge = (left, right) => {
+    const combinedArraysLength = left.length + right.length;
+    ;
+    let result = [], leftIndex = 0, rightIndex = 0;
+    for (let index = 0; index < combinedArraysLength; index++) {
+        const leftComparedValue = left[leftIndex], rightComparedValue = right[rightIndex];
+        if (leftComparedValue === undefined) {
+            const restOfTheRemainingArray = right.slice(rightIndex);
+            result = result.concat(restOfTheRemainingArray);
+            break;
+        }
+        if (rightComparedValue === undefined) {
+            const restOfTheRemainingArray = left.slice(leftIndex);
+            result = result.concat(restOfTheRemainingArray);
+            break;
+        }
+        if (leftComparedValue < rightComparedValue) {
+            result.push(leftComparedValue);
+            leftIndex++;
+        }
+        else {
+            result.push(rightComparedValue);
+            rightIndex++;
+        }
+    }
+    return result;
+};
 export default mergeSort;
 //# sourceMappingURL=index.js.map
--- merge-sort/index.ts
+++ merge-sort/index.ts
@@ -1,7 +1,63 @@
 'use strict';
 
-function mergeSort(input: Array<number>): Array<number> {
-  return input;
+const BASE_CASE_SIZE = 1;
+
+const mergeSort = (input: Array<number>): Array<number> => {
+  const
+    inputLength: number = input.length,
+    inputIsSmallEnough: boolean = inputLength <= BASE_CASE_SIZE;
+
+  if (inputIsSmallEnough) {
+    return input;
+  }
+
+  // Split the input
+  const
+    splittingPoint: number = Math.floor(inputLength / 2),
+
+    leftArray = input.slice(0, splittingPoint),
+    rightArray = input.slice(splittingPoint),
+
+    sortedLeftArray = mergeSort(leftArray),
+    sortedRightArray = mergeSort(rightArray);
+
+  return merge(sortedLeftArray, sortedRightArray);
 };
+
+const merge = (left: Array<number>, right: Array<number>): Array<number> => {
+  const combinedArraysLength = left.length + right.length;;
+  let
+    result: Array<number> = [],
+    leftIndex: number = 0,
+    rightIndex: number = 0;
+
+  for (let index = 0; index < combinedArraysLength; index++) {
+    const
+      leftComparedValue = left[leftIndex],
+      rightComparedValue = right[rightIndex];
+
+    if (leftComparedValue === undefined) {
+      const restOfTheRemainingArray = right.slice(rightIndex);
+      result = result.concat(restOfTheRemainingArray);
+      break;
+    }
+
+    if (rightComparedValue === undefined) {
+      const restOfTheRemainingArray = left.slice(leftIndex);
+      result = result.concat(restOfTheRemainingArray);
+      break;
+    }
+
+    if (leftComparedValue < rightComparedValue) {
+      result.push(leftComparedValue);
+      leftIndex++;
+    } else {
+      result.push(rightComparedValue);
+      rightIndex++;
+    }
+  }
+
+  return result;
+}
 
 export default mergeSort;
